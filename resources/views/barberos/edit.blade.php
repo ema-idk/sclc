919132e934cc40bdb5fa63c42de5f9d74af4f38d
ema_idk <67031254+ema-idk@users.noreply.github.com>
2021-04-16 20:26:12 -0700

    Se realizo un cambio en el form

--- resources/views/barberos/edit.blade.php
+++ resources/views/barberos/edit.blade.php
@@ -1,12 +1,13 @@
-<form action="{{url('/barbero/' . $barbero->id)}}" method="post" enctype="multipart/form-data">
-{{csrf_field()}}
-    {{method_field('PATCH')}}
+<form action="{{ url('/barberos/' . $barbero->id) }}" method="post">
+    {{ csrf_field() }}
+    {{ method_field('PATCH') }}
     <label for="Nombre">{{'Nombre completo'}}</label>
-<input type="text" name="Nombre" id="Nombre" value="{{$barbero->nombre}}">
-<br/>
+    <input type="text" name="Nombre" id="Nombre" value="{{$barbero->nombre}}">
+    <br/>
 
-<label for="Estado">{{'Estado'}}</label>
+    <label for="Estado">{{'Estado'}}</label>
     <input type="text" name="Estado" id="Estado" value="{{$barbero->estado}}">
     <br/>
-<input type="submit" value="Actualizar informacion">
+
+    <input type="submit" value="Actualizar informacion">
 </form>
